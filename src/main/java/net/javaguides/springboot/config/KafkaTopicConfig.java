8ffea8f0d10a9e10f639f4ed839f7e321d7934f7
Raúl Pérez Vicente <rperezv@ayesa.com>
2026-08-30 20:07:25 +0200

    273. Create Kafka Producer

---
+++ src/main/java/net/javaguides/springboot/config/KafkaTopicConfig.java
@@ -0,0 +1,26 @@
+package net.javaguides.springboot.config;
+
+import org.apache.kafka.clients.admin.NewTopic;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.config.TopicBuilder;
+
+/**
+ * KafkaTopicConfig
+ * <p>
+ * Created by IntelliJ, Spring Framework Guru.
+ *
+ * @author architecture - pvraul
+ * @version 30/08/2026 - 20:04
+ * @since 1.17
+ */
+@Configuration
+public class KafkaTopicConfig {
+
+    @Bean
+    public NewTopic javaguidesTopic() {
+        return TopicBuilder.name("javaguides")
+                .build();
+    }
+
+}
