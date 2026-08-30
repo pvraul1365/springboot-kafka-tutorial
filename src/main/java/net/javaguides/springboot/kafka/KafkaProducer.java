6f20a39225ad44a9d9183bbbe587ef38f88f9dde
Raúl Pérez Vicente <rperezv@ayesa.com>
2026-08-30 20:21:02 +0200

    274. Create Kafka Producer

---
+++ src/main/java/net/javaguides/springboot/kafka/KafkaProducer.java
@@ -0,0 +1,34 @@
+package net.javaguides.springboot.kafka;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
+
+/**
+ * KafkaProducer
+ * <p>
+ * Created by IntelliJ, Spring Framework Guru.
+ *
+ * @author architecture - pvraul
+ * @version 30/08/2026 - 20:14
+ * @since 1.17
+ */
+@Service
+public class KafkaProducer {
+
+    private static final Logger log = LoggerFactory.getLogger(KafkaProducer.class);
+
+    private final KafkaTemplate<String, String> kafkaTemplate;
+
+    public KafkaProducer(KafkaTemplate<String, String> kafkaTemplate) {
+        this.kafkaTemplate = kafkaTemplate;
+    }
+
+    public void sendMessage(final String message) {
+        log.info("Sending message='{}' to topic='javaguides'", message);
+        
+        kafkaTemplate.send("javaguides", message);
+    }
+
+}
