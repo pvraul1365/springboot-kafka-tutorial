18c684f23567b503278572e7b2cd0fb73bc23d13
Raúl Pérez Vicente <rperezv@ayesa.com>
2026-08-31 08:20:50 +0200

    276. Create Kafka Consumer

---
+++ src/main/java/net/javaguides/springboot/kafka/KafkaConsumer.java
@@ -0,0 +1,36 @@
+package net.javaguides.springboot.kafka;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
+
+/**
+ * KafkaConsumer
+ * <p>
+ * Created by IntelliJ, Spring Framework Guru.
+ *
+ * @author architecture - pvraul
+ * @version 31/08/2026 - 08:10
+ * @since 1.17
+ */
+@Service
+public class KafkaConsumer {
+
+    private static final Logger log = LoggerFactory.getLogger(KafkaConsumer.class);
+
+    private final KafkaTemplate<String, String> kafkaTemplate;
+
+    public KafkaConsumer(KafkaTemplate<String, String> kafkaTemplate) {
+        this.kafkaTemplate = kafkaTemplate;
+    }
+
+    // Add your Kafka listener methods here
+    @KafkaListener(topics = "javaguides", groupId = "my-group")
+    public void consume(final String message) {
+        // Process the consumed message
+        log.info("Consumed message: {}", message);
+    }
+
+}
